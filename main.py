f887c9410ce7d30f5cd9d9e6ff7b1b3f34d51779
Erchis Patwardhan <erchispatwardhan@berkeley.edu>
2023-10-17 16:29:55 -0700

    refactored mongo connection

--- main.py
+++ main.py
@@ -4,34 +4,37 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = os.getenv("MONGODB_URI")
 
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+def connect_to_mongo():
+    uri = os.getenv("MONGODB_URI")
 
-# Send a ping to confirm a successful connection
-x = "It didn't work :("
-try:
-    client.admin.command('ping')
-    x = "It worked :)"
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+    global client
+    global x1
+
+    # Create a new client and connect to the server
+    client = MongoClient(uri, server_api=ServerApi('1'))
+
+    # Send a ping to confirm a successful connection
+    x1 = "It didn't work :("
+    try:
+        client.admin.command('ping')
+        x1 = "It worked :)"
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+
+connect_to_mongo()
 
 
 @app.get("/")
 async def root():
-
-
-    return {"message": x}
+    return {"message": x1}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
-
-
