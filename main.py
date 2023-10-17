7c071aad0423ac758c25f4299d7f7939eb3b9e7c
Erchis Patwardhan <erchispatwardhan@berkeley.edu>
2023-10-17 16:17:33 -0700

    added mongodb connection

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+import os
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = os.getenv("MONGODB_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+x = 1
+try:
+    client.admin.command('ping')
+    x = 1
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello new world, all the boys and girls, I got some true stories to tell"}
+    return {"message": "Hello new world, all the boys and girls, I got some true stories to tell", x}
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
