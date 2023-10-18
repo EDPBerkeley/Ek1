0901e31c004ef08ac787b254caaed5db305b74ba
Erchis Patwardhan <erchispatwardhan@berkeley.edu>
2023-10-17 17:46:21 -0700

    this one's for min

--- app/main.py
+++ app/main.py
@@ -7,27 +7,13 @@ app = FastAPI()
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
+global client
+global x1
 
-def connect_to_mongo():
-    uri = os.getenv("MONGODB_URI")
-
-    global client
-    global x1
-
-    # Create a new client and connect to the server
-    client = MongoClient(uri, server_api=ServerApi('1'))
-
-    # Send a ping to confirm a successful connection
-    x1 = "It didn't work :("
-    try:
-        client.admin.command('ping')
-        x1 = "It worked :)"
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-    except Exception as e:
-        print(e)
-
-
-connect_to_mongo()
+try:
+    client = MongoClient(os.getenv("MONGODB_URI"), server_api=ServerApi('1'))
+except:
+    print("Couldn't connect to DB")
 
 
 @app.get("/")
--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -1,9 +1,32 @@
-from app.main import connect_to_mongo
+import random
+from datetime import datetime, timedelta
+from app.main import client
+
+from app.models.store import Store
 
 
-client: str
-connect_to_mongo()
+def create_stores():
+    db = client["app"]
+    collection = db["users"]
+
+    collection.delete_many({})
+
+    # Define the range of the last 5 years (in days)
+    start_date = datetime.now() - timedelta(days=365 * 5)
+    end_date = datetime.now()
+
+    for _ in range(20):  # You can specify the number of random dates you want
+        random_days = random.randint(0, (end_date - start_date).days)
+        random_date = start_date + timedelta(days=random_days)
+
+        store = Store(date_created=random_date)
+
+        try:
+            store.save()
+            print(f"store '{store.id}' saved to db")
+
+        except:
+            print(f"store '{store.id}' could not be saved to db")
 
 
-# def create_users(d)
-
+create_stores()
